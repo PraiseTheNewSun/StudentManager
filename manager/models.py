aa4372fe466b9e7042171620f09768e6ec10dada
PraiseTheNewSun <142599118+PraiseTheNewSun@users.noreply.github.com>
2025-02-12 14:23:10 +0100

    Log in setup

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class StudentInfo(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    level = models.IntegerField()
+    address = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
+
+    def __str__(self):
+        return self.first_name + self.last_name
